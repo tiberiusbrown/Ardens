536f2aad7a2f852fed1da55ee0f926115ab05ac8
Peter Brown <brow1067@gmail.com>
2023-02-09 13:36:48 -0700

    Update window_symbols.cpp

--- src/window_symbols.cpp
+++ src/window_symbols.cpp
@@ -18,6 +18,7 @@ void window_symbols(bool& open)
     if(!open) return;
     if(Begin("Symbols", &open) && arduboy.cpu.decoded && arduboy.elf)
     {
+        AlignTextToFramePadding();
         TextUnformatted("Show:");
         SameLine();
         Checkbox("Objects", &show_objects);
@@ -35,7 +36,12 @@ void window_symbols(bool& open)
                 "  \"-xxx\"     hide lines containing \"xxx\"");
             EndTooltip();
         }
-        if(BeginListBox("##symbols", { -1, -1 }))
+
+        ImGuiTableFlags flags = 0;
+        flags |= ImGuiTableFlags_ScrollY;
+        flags |= ImGuiTableFlags_RowBg;
+        flags |= ImGuiTableFlags_NoClip;
+        if(BeginTable("##symbols", 1, flags, { -1, -1 }))
         {
             for(uint16_t addr : arduboy.elf->text_symbols_sorted)
             {
@@ -43,11 +49,13 @@ void window_symbols(bool& open)
                 if(!show_objects && sym.object) continue;
                 if(!show_labels && sym.notype) continue;
                 if(!filter.PassFilter(sym.name.c_str())) continue;
+                TableNextRow();
+                TableSetColumnIndex(0);
                 if(Selectable(sym.name.c_str()))
                     disassembly_scroll_addr = addr, scroll_addr_to_top = true;
             }
 
-            EndListBox();
+            EndTable();
         }
     }
     End();
