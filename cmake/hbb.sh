2f813e932d76abf4722602fabdf31a85d99d334e
Peter Brown <brow1067@gmail.com>
2023-11-02 19:21:17 -0600

    Revert "Use Ninja for Linux build"
    This reverts commit 2eea565cea5a5408a4ace079ee3231935a267c78.

--- cmake/hbb.sh
+++ cmake/hbb.sh
@@ -9,7 +9,7 @@ source /hbb_exe_gc_hardened/activate
 
 set -x
 
-yum -y install python3 libX11-devel libXi-devel libXcursor-devel pulseaudio-libs-devel alsa-lib-devel mesa-libGL-devel ninja-build
+yum -y install python3 libX11-devel libXi-devel libXcursor-devel pulseaudio-libs-devel alsa-lib-devel mesa-libGL-devel
 
 nj=$(nproc)
 if [[ $nj > 4 ]]; then
@@ -18,8 +18,8 @@ fi
 
 mkdir build
 cd build
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DARDENS_LLVM=1 -DARDENS_DEBUGGER=1 -DARDENS_PLAYER=1 -DARDENS_LIBRETRO=0 /io
-cmake --build .
+cmake -DCMAKE_BUILD_TYPE=Release -DARDENS_LLVM=1 -DARDENS_DEBUGGER=1 -DARDENS_PLAYER=1 -DARDENS_LIBRETRO=0 /io
+make -j$nj
 strip -x Ardens ArdensPlayer
 cp Ardens ArdensPlayer /io/build
 
@@ -30,8 +30,8 @@ source /hbb_shlib/activate
 
 mkdir build
 cd build
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DARDENS_LLVM=0 -DARDENS_DEBUGGER=0 -DARDENS_PLAYER=0 -DARDENS_LIBRETRO=1 /io
-cmake --build .
+cmake -DCMAKE_BUILD_TYPE=Release -DARDENS_LLVM=0 -DARDENS_DEBUGGER=0 -DARDENS_PLAYER=0 -DARDENS_LIBRETRO=1 /io
+make -j$nj
 strip -x ardens_libretro.so
 cp ardens_libretro.so /io/build
 
