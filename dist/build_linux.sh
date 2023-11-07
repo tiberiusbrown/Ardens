1e35b70e0195c500854756194937318f0a7e9346
tiberiusbrown <brow1067@gmail.com>
2023-11-07 11:17:21 -0800

    Clean up

--- dist/build_linux.sh
+++ dist/build_linux.sh
@@ -26,11 +26,3 @@ ln -s "${file}" AppDir/AppRun
 cd ..
 ./build/linuxdeploy-x86_64.AppImage --appdir build/AppDir --output appimage
 
-#rm -rf build
-#mkdir -p build
-#cmake -B build .. -DARDENS_LLVM=0 -DARDENS_DEBUGGER=0 -DARDENS_PLAYER=0 -DARDENS_DIST=1 -DCMAKE_BUILD_TYPE=Release
-#cmake --build build -j $(nproc)
-#for f in $(ls build/*); do
-#	[[ -x "$f" ]] && cp "$f" .
-#done
-
