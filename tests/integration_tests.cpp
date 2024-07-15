fd412fdf8320c14aaec6ada95ed872055c7c99b9
Peter Brown <brow1067@gmail.com>
2024-07-14 20:21:10 -0600

    Update integration_tests.cpp

--- tests/integration_tests.cpp
+++ tests/integration_tests.cpp
@@ -17,7 +17,7 @@ static int test(char const* name)
     auto err = arduboy->load_file("test.hex", f);
     arduboy->reset();
     if(err.empty())
-        arduboy->advance(100'000'000'000ull); // 100 ms
+        arduboy->advance(1'000'000'000'000ull); // one second
 
     auto const& d = arduboy->cpu.serial_bytes;
     bool pass = (d.size() == 1 && d[0] == 'P');
