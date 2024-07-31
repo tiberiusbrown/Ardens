18c4f53d546ce176a2b848349ac9e875a06516f1
Peter Brown <brow1067@gmail.com>
2024-07-31 16:59:04 -0600

    SIMD display pixel filtering on x64

--- src/absim.hpp
+++ src/absim.hpp
@@ -29,6 +29,16 @@
 #define ARDENS_FORCEINLINE
 #endif
 
+#if defined(__x86_64__) || defined(_M_X64)
+#define ARDENS_ARCH_X86_64
+#elif defined(i386) || defined(__i386__) || defined(__i386) || defined(_M_IX86)
+#define ARDENS_ARCH_X86_32
+#endif
+
+#ifdef ARDENS_ARCH_X86_64
+#define ARDENS_SSE2
+#endif
+
 #ifdef ARDENS_LLVM
 namespace llvm
 {
